805a7756faa449e1eb7bb095d113e716da8fceaa
SriyaBL <87294793+SriyaBL@users.noreply.github.com>
2023-06-17 14:09:18 +0530

    made changes

--- script.js
+++ script.js
@@ -6,6 +6,18 @@ for (let i = 0; i < 9; i++) {
         const cell = document.createElement('input');
         cell.type = 'number';
         cell.className = 'sudoku-cell';
+
+        let idx = i * 9 + j;
+        if (
+            ((idx % 9 == 0 || idx % 9 == 1 || idx % 9 == 2) && idx < 21) ||
+            ((idx % 9 == 6 || idx % 9 == 7 || idx % 9 == 8) && idx < 27) ||
+            ((idx % 9 == 3 || idx % 9 == 4 || idx % 9 == 5) && (idx > 27 && idx < 53)) ||
+            ((idx % 9 == 0 || idx % 9 == 1 || idx % 9 == 2) && idx > 53) ||
+            ((idx % 9 == 6 || idx % 9 == 7 || idx % 9 == 8) && idx > 53)
+        ) {
+            cell.classList.add('odd-section');
+        }
+
         sudokuGrid.appendChild(cell);
     }
 }
@@ -36,11 +48,12 @@ function solveSudoku() {
                 return;
             }
             matrix[i][j] = cellValue;
+
         }
     }
 
     if (!validInput(matrix)) {
-        alert('Invalid input!');
+        alert('No solution possible!');
         clearSudoku();
         return;
     }
@@ -52,7 +65,6 @@ function solveSudoku() {
         for (let j = 0; j < 9; j++) {
             const cellIndex = i * 9 + j;
             sudokuCells[cellIndex].value = matrix[i][j];
-
         }
     }
 
